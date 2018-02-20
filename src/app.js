563ab17db17e2aad60e4fc7e1dcd5d5d9725df19
Charlie Heiner <heinercj@gmail.com>
2018-02-20 10:34:39 -0800

    can same meme generated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
+import dom2image from 'dom-to-image';
+import fileSaver from 'file-saver';
 
 export default class App extends Component{
 
@@ -7,13 +9,15 @@ export default class App extends Component{
         super();
 
         this.state = {
-            memeText: "hello",
+            memeText: "",
         }
 
         this.handleMemeText = this.handleMemeText.bind(this);
         this.handleColor = this.handleColor.bind(this);
         this.handleSize = this.handleSize.bind(this);
         this.handleUpload = this.handleUpload.bind(this);
+        this.handleURL = this.handleURL.bind(this)
+        this.handleExport = this.handleExport.bind(this)
     }
 
     handleMemeText({ target }){
@@ -34,6 +38,12 @@ export default class App extends Component{
         })
     }
 
+    handleURL( {target }){
+        this.setState({
+            background: target.value
+        })
+    }
+
     handleUpload( { target }){
         const reader = new FileReader();
 
@@ -43,6 +53,12 @@ export default class App extends Component{
             this.setState({ background: reader.result })
         }
     }
+
+    handleExport(){
+        dom2image.toBlob(this.section).then(blob => {
+            fileSaver.saveAs(blob, 'meme.png');
+        })
+    }
     
 
     render(){
@@ -50,29 +66,40 @@ export default class App extends Component{
         return(
             <section>
                 <h1>Meme Generator</h1>
+                <div className="options">
+                    <div>
+                        <label>Your text: </label>
+                        <input type="text" onChange={this.handleMemeText}/>
 
-                <label>Your text: </label>
-                <input type="text" onChange={this.handleMemeText}/>
-
-                <label>Upload Background:</label>
-                <input type="file" onChange={this.handleUpload}/>
-                <label>
-                    Text Color:
-                    <select onChange={this.handleColor}>
-                        <option>Black</option>
-                        <option>White</option>
-                    <   option>Green</option>
-                    </select>
-                    Font Size:
-                    <select onChange={this.handleSize}>
-                        <option>20</option>
-                        <option>30</option>
-                        <option>40</option>
-                    </select>
-                </label>
-
+                        <label>Upload Image URL or Image:</label>
+                        <input name="url" onChange={this.handleURL}/>
+                        <input type="file" onChange={this.handleUpload}/>
+                    </div>
+                    <div>
+                        <button onClick={this.handleExport}>Save</button>
+                    </div>
+                    <div>
+                        Text Color:
+                        <select onChange={this.handleColor}>
+                            <option>Black</option>
+                            <option>White</option>
+                            <option>Green</option>
+                            <option>Red</option>
+                            <option>Purple</option>
+                        </select>
+                        Font Size:
+                        <select onChange={this.handleSize}>
+                            <option>20</option>
+                            <option>30</option>
+                            <option>40</option>
+                            <option>60</option>
+                            <option>80</option>
+                        </select>  
+                    </div>
+                </div>
 
                 <div
+                className="meme"
                 ref={node => this.section = node}
                 style={{
                     color: color,
@@ -82,6 +109,7 @@ export default class App extends Component{
                 >
                     {memeText}
                 </div>
+
             </section>
         )
    }
