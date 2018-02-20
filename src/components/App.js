37a8db622d127d257d119f23d9bf0676c9bf9756
G.Provost <grace.g.provost@gmail.com>
2018-02-19 20:27:45 -0800

    add react

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,8 @@
-console.log(() => 'this came from App.js!');
+import React,{ Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return <h1>This is an app!</h1>;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './main.css';
 import App from './components/App';
 
-// ReactDOM.render(
-//   <App/>, // react will call new App()
-//   document.getElementById('root')
-// );
-
-console.log('main.js sent this!');
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
