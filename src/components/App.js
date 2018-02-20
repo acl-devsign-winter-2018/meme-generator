3d48f832d1df0d179dcaeb1f4124f5052ec4c0fb
G.Provost <grace.g.provost@gmail.com>
2018-02-20 10:29:30 -0800

    add many funtionalities and styling

--- src/components/App.js
+++ src/components/App.js
@@ -10,23 +10,36 @@ export default class App extends Component {
 
     this.state = {
       header: "upload a picture!",
-      footer: "write some meme text!",
+      footer: "write some text!",
       color: "red",
+      size: "1rem",
       font: 'Helvetica',
       background: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/A_blank_flag.png'
     }
     
     this.fonts = ['Helvetica', 'Arial', 'Times', 'Courier', 'Tahoma', 'Garamond', 'Impact']
 
+    this.sizes = ['.5rem', '1rem', '1.25rem', '1.5rem', '1.75rem', '2rem', '2.5rem', '3rem']
+
     this.handleFontChange = this.handleFontChange.bind(this);
     this.handleExport = this.handleExport.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
     this.handleUrl = this.handleUrl.bind(this);
+    this.handleHeader = this.handleHeader.bind(this);
+    this.handleFooter = this.handleFooter.bind(this);
+    this.handleColor = this.handleColor.bind(this);
+    this.handleSizeChange = this.handleSizeChange.bind(this);
   }
 
   handleFontChange({target}) {
     this.setState({
       font: target.value || 'Helvetica'
+    })
+  }
+
+  handleSizeChange({target}) {
+    this.setState({
+      size: target.value || '1rem'
     })
   }
 
@@ -44,62 +57,106 @@ export default class App extends Component {
     reader.onload = () => {
       this.setState({ background: reader.result });
     };
+    target.value = "";
   }
 
   handleUrl({target}) {
     this.setState({
       background: target.value
     });
+    target.value = "";
+  }
+
+  handleHeader({target}) {
+    this.setState({
+      header: target.value
+    });
+  }
+
+  handleFooter({target}) {
+    this.setState({
+      footer: target.value
+    });
+  }
+
+  handleColor({target}) {
+    this.setState({
+      color: target.value
+    });
   }
   
-  
   render() {
-    const { header, footer, color, font, background } = this.state;
+    const { header, footer, color, font, background, size } = this.state;
 
     return (
       <main id="main">
         <header>
           <h1>Meme Generator</h1>
         </header>
-
-        <div id="label-holder">
-          <label>
-            Upload an image:
-            <input type="file" onChange={this.handleUpload}/>
-          </label>
-          <label>
-            Input URL:
-            <input type="url" onChange={this.handleUrl}/>
-          </label>
-          <label>
-            Select a font:
-              <select onChange={this.handleFontChange}>
-                <option value="">Select font</option>
-                {this.fonts.map((font, index) => (
-                  <option value={font} key={index}>{font}</option>
-                ))}
-              </select>
-          </label>
+        <div id="grid">
+          <div id="label-holder">
+            <label>
+              Upload an image:
+              <input type="file" onChange={this.handleUpload}/>
+            </label>
+            <label>
+              Input URL:
+              <input type="url" onChange={this.handleUrl} placeholder="Ex: https://backgroundimage.jpg"/>
+            </label>
+            <label>
+              Edit Header:
+              <input type="text" onChange={this.handleHeader} placeholder="upload a picture!"/>
+            </label>
+            <label>
+              Edit Footer:
+              <input type="text" onChange={this.handleFooter} placeholder="write some text!"/>
+            </label>
+            <label>
+              Change Font Color:
+              <input id="color" type="color" onChange={this.handleColor}/>
+            </label>
+            <label>
+              Select a Font:
+                <select onChange={this.handleFontChange}>
+                  <option value="">Select font</option>
+                  {this.fonts.map((font, index) => (
+                    <option value={font} key={index}>{font}</option>
+                  ))}
+                </select>
+            </label>
+            <label>
+              Select a Font-Size:
+                <select onChange={this.handleSizeChange}>
+                  <option value="">Select size</option>
+                  {this.sizes.map((size, index) => (
+                    <option value={size} key={index}>{size}</option>
+                  ))}
+                </select>
+            </label>
+          </div>
+          
+          <figure
+            ref={node => this.figure = node}
+          >
+            <img src={background}/>
+            <figcaption 
+              style={{
+              fontFamily: `${font}`,
+              color: `${color}`,
+              fontSize: `${size}`
+              }} 
+              id="header"
+              >{header}</figcaption>
+            <figcaption 
+              style={{
+              fontFamily: `${font}`,
+              color: `${color}`,
+              fontSize: `${size}`
+              }} 
+              id="footer"
+              >{footer}</figcaption>
+          </figure>
         </div>
-        
-        <figure
-          ref={node => this.figure = node}
-        >
-          <img src={background}/>
-          <figcaption 
-            style={{
-            fontFamily: `${font}`
-            }} 
-            id="header"
-            >{header}</figcaption>
-          <figcaption 
-            style={{
-            fontFamily: `${font}`
-            }} 
-            id="footer"
-            >{footer}</figcaption>
-        </figure>
-
         <button onClick={this.handleExport}>Save Meme</button>
       </main>
     
