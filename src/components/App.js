7307aa5478fd5c6c468b7fab0320bda9032427b5
G.Provost <grace.g.provost@gmail.com>
2018-02-19 21:35:40 -0800

    start adding font change to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,55 @@
 import React, { Component } from 'react';
+import './app.css';
 
 export default class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      header: "upload a picture!",
+      footer: "write some meme text!",
+      color: "red",
+      font: 'Helvetica'
+    }
+    
+    this.fonts = ['Helvetica', 'Arial', 'Times', 'Courier', 'Tahoma', 'Garamond', 'Impact']
+
+    this.handleFontChange = this.handleFontChange.bind(this);
+  }
+
+  handleFontChange({target}) {
+    this.setState({
+      font: target.value || 'Helvetica'
+    })
+  }
+  
+  
   render() {
-    return <h1>This is an app!</h1>;
+    const { header, footer, color, font, fonts } = this.state;
+
+    return (
+      <main id="main">
+        <header>
+          <h1 
+          style={{
+            fontFamily: {font}
+          }}>Meme Generator</h1>
+        </header>
+
+        <form>
+          <label>
+            Select a font:
+              <select onChange={this.handleFontChange}>
+                <option value="">Select font</option>
+                {this.fonts.map((font, index) => (
+                  <option value={font} key={index}>{font}</option>
+                ))}
+              </select>
+          </label>
+        </form>
+      </main>
+    
+    );
   }
 }
