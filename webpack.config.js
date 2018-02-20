e9579f5d59fa2f2ec7ccd15f24ab74f4922fbcda
Charlie Heiner <heinercj@gmail.com>
2018-02-19 19:31:13 -0800

    adds devtool inline source map.  index template

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const HtmlPlugin = require('html-webpack-plugin');
 
 const build = `${__dirname}/build`;
 
@@ -12,8 +12,10 @@ module.exports = {
     devServer: {
         contentBase: './build',
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(build),
+        new HtmlPlugin({ template: './src/index.html' }),
     ]
 
 }
